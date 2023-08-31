17aa522bd4d76c42dc4dcc5fbc4f927c042866e3
Nik <nnepochatyy@mail.ru>
2023-08-31 21:14:49 +0500

    Improved rendering performance: -made some debug methods; -improved rendering by handling image scaling properly; -made new scale methods for all tiles and player models;

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -91,6 +91,12 @@ public class GamePanel extends JPanel implements Runnable {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D)g;
 
+        // Debug
+        long drawStart = 0;
+        if(keyH.checkDrawTime) {
+            drawStart = System.nanoTime();
+        }
+
         // Tile
         tileM.draw(g2);
 
@@ -106,6 +112,15 @@ public class GamePanel extends JPanel implements Runnable {
 
         // UI
         ui.draw(g2);
+
+        // Debug
+        if(keyH.checkDrawTime) {
+            long drawEnd = System.nanoTime();
+            long passed = drawEnd - drawStart;
+            g2.setColor(Color.white);
+            g2.drawString("Draw time: " + passed, 10, 400);
+            System.out.println("Draw time: " + passed);
+        }
 
         g2.dispose();
     }
--- src/main/UI.java
+++ src/main/UI.java
@@ -20,7 +20,7 @@ public class UI {
 
         arial_40 = new Font("Arial", Font.PLAIN, 40);
         arial_80B = new Font("Arial", Font.BOLD, 80);
-        Key key = new Key();
+        Key key = new Key(gp);
         keyImage = key.image;
     }
 
